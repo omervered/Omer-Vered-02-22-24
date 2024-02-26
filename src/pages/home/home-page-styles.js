8bed6cd11d40a48b9482ed0f8b8afc786344c608
Omer Vered <omervered21@gmail.com>
2024-02-26 16:47:34 +0200

    removed comments

--- src/pages/home/home-page-styles.js
+++ src/pages/home/home-page-styles.js
@@ -19,7 +19,6 @@ export const HomePageContainer = styled.div`
   justify-content: center;
   margin: 50px 25px;
   padding: 50px;
-  /* background-color: ${colors["alice-blue"]}; */
   background-color: ${(props) => props.theme.containerBackground};
   box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.2);
   border: 2px solid ${colors["alice-blue-dark"]};
